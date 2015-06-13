5b419889927d99412076b3d9cbab00854edfef49
WULIANSHU <718153754@qq.com>
2015-06-14 01:21:54 +0800

    吴联暑

---
+++ AnimationProject/src/com/ap/MyTest2.java
@@ -0,0 +1,13 @@
+package com.ap;
+/**
+ * 
+ * @author wls
+ *
+ */
+public class MyTest2 extends Thread {
+  @Override
+public void run() {
+	// TODO Auto-generated method stub
+	super.run();
+}
+}
